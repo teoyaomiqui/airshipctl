1a856946c44ec3fa23a8cf4b5c0ade7dbc625aa1
Rodolfo Pacheco <jezogwza@gmail.com>
2019-12-04 17:19:06 -0500

    Introduces set_context and get_context operations
    Each of these include an option for --current-context that set or 
    retrieves the curret context
    
    This commit:
    
     - creates the cmd/config and pkg/config required additions.
     - Includes a fync getCurrentContext(<CLUSTERTYPE>)  in the config pkg
    that other modules should rely on.
     - Introduces new  ErrMissingConfig and ErrConfigFailed types been
     used by set-context. Will repeat the use through get_context as well as
     set/get cluster after this is reviewed.
    
    Change-Id: I501483a9db99f33f860eaf329a65bb0209b2aaff

--- pkg/errors/common.go
+++ pkg/errors/common.go
@@ -1,5 +1,18 @@
 package errors
 
+// AirshipError is the base error type
+// used to create extended error types
+// in other airshipctl packages.
+type AirshipError struct {
+	Message string
+}
+
+// Error function implments the golang
+// error interface
+func (ae *AirshipError) Error() string {
+	return ae.Message
+}
+
 // ErrNotImplemented returned for not implemented features
 type ErrNotImplemented struct {
 }
@@ -14,4 +27,20 @@ type ErrWrongConfig struct {
 
 func (e ErrWrongConfig) Error() string {
 	return "Error. Wrong configuration"
+}
+
+// ErrMissingConfig returned in case of missing configuration
+type ErrMissingConfig struct {
+}
+
+func (e ErrMissingConfig) Error() string {
+	return "Error. Missing configuration"
+}
+
+// ErrConfigFailed returned in case of failure during configuration
+type ErrConfigFailed struct {
+}
+
+func (e ErrConfigFailed) Error() string {
+	return "Error. Configuration failed to complete."
 }
